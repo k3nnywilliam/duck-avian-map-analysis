9fa6113bbdeee02bab438afedc5c57fb21d04d11
k3nnywilliam <k3nnywilliam@gmail.com>
2021-10-13 11:42:32 +0800

    Add custom mapbox map and icons

--- public/src/map.js
+++ public/src/map.js
@@ -1,11 +1,13 @@
+
+
 let mapOptions = {
     center: [29.17384, 116.27890],
     zoom: 10
 }
 let map = new L.map('mapid', mapOptions);
-let layer = new L.TileLayer('http://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png');
+let layer = new L.TileLayer(`https://api.mapbox.com/styles/v1/kennyallau/ckuoxocix5u8s17nsv1gpudg6/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1Ijoia2VubnlhbGxhdSIsImEiOiJja3VveHd3Z3UwMmMxMm9xdm1uMWhlaGlwIn0.5IWi1IBZe3-ypfrW-0jivA`);
 
-var geojsonMarkerOptions = {
+/* var geojsonMarkerOptions = {
     radius: 8,
     fillColor: "#ff7800",
     color: "#000",
@@ -18,17 +20,39 @@ L.geoJSON(geoFeaturePoints, {
     pointToLayer: function (feature, latlng) {
         return L.circleMarker(latlng, geojsonMarkerOptions);
     }
-}).addTo(map);
+}).addTo(map); */
+
+/* 
+L.Control.Watermark = L.Control.extend({
+    onAdd: function(map) {
+        var img = L.DomUtil.create('img');
+
+        img.src = 'src/img/mallard.png';
+        img.style.width = '200px';
+
+        return img;
+    },
+
+    onRemove: function(map) {
+        // Nothing to do here
+    }
+});
+
+L.control.watermark = function(opts) {
+    return new L.Control.Watermark(opts);
+}
+
+L.control.watermark({ position: 'bottomleft' }).addTo(map); */
 
 
-/* var LeafIcon = L.Icon.extend({
+var LeafIcon = L.Icon.extend({
     options: {
-        iconSize: [10, 10],
+        iconSize: [20, 20],
         iconAnchor: [22, 94]
     }
 });
 
-var mallard = new LeafIcon({ iconUrl: 'img/mallard.png' });
+var mallard = new LeafIcon({ iconUrl: 'src/img/mallard-duck.png' });
 
 // custom duck marker
 L.geoJSON(geoFeaturePoints, {
@@ -37,7 +61,5 @@ L.geoJSON(geoFeaturePoints, {
     }
 }).addTo(map);
 
- */
-// L.geoJSON(geoFeaturePoints).addTo(map);
-//  L.geoJSON(geoFeatureLines).addTo(map);
+
 map.addLayer(layer);
