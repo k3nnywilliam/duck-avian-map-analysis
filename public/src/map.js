ade77763d947811ee196fe08ce66dd87f4c2d787
k3nnywilliam <k3nnywilliam@gmail.com>
2021-10-13 23:53:17 +0800

    Update geofeature lines, add muscovy icon, update return values for feature points

--- public/src/map.js
+++ public/src/map.js
@@ -7,51 +7,12 @@ let mapOptions = {
 let map = new L.map('mapid', mapOptions);
 let layer = new L.TileLayer(`https://api.mapbox.com/styles/v1/kennyallau/ckuoxocix5u8s17nsv1gpudg6/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1Ijoia2VubnlhbGxhdSIsImEiOiJja3VveHd3Z3UwMmMxMm9xdm1uMWhlaGlwIn0.5IWi1IBZe3-ypfrW-0jivA`);
 
-/* var geojsonMarkerOptions = {
-    radius: 8,
-    fillColor: "#ff7800",
-    color: "#000",
-    weight: 1,
-    opacity: 1,
-    fillOpacity: 0.8
-};
-
-L.geoJSON(geoFeaturePoints, {
-    pointToLayer: function (feature, latlng) {
-        return L.circleMarker(latlng, geojsonMarkerOptions);
-    }
-}).addTo(map); */
-
-/* 
-L.Control.Watermark = L.Control.extend({
-    onAdd: function(map) {
-        var img = L.DomUtil.create('img');
-
-        img.src = 'src/img/mallard.png';
-        img.style.width = '200px';
-
-        return img;
-    },
-
-    onRemove: function(map) {
-        // Nothing to do here
-    }
-});
-
-L.control.watermark = function(opts) {
-    return new L.Control.Watermark(opts);
-}
-
-L.control.watermark({ position: 'bottomleft' }).addTo(map); */
-
 
 let LeafIcon = L.Icon.extend({
     options: {
-        shadowUrl: 'src/img/mallard-duck-shadow2.png',
-        iconSize: [20, 20],
-        shadowSize:   [22, 22],
-        shadowAnchor: [22, 94],
-        iconAnchor: [22, 94]
+        iconSize: [30, 30],
+       /*  shadowAnchor: [0, 0],
+        iconAnchor: [0, 0] */
     }
 });
 
@@ -69,14 +30,6 @@ var linestyle = {
     weight: 7,
     opacity: 0.4
     };
-
-/* // create geolines
-L.geoJSON(geoFeaturesLines, {
-    pointToLayer: function (feature, latlng) {
-        return L.polyline(latlng, { linestyle });
-    }
-}).addTo(map);
- */
 
 L.geoJSON(geoFeaturesLines, {
     style: linestyle
