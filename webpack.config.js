ffef28911aeda109bdece9e881d9a44aecb8502d
Kenny Nyallau <k3nnywilliam@gmail.com>
2021-10-28 17:22:09 +0800

    Add webpack config file

---
+++ webpack.config.js
@@ -0,0 +1,81 @@
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  devServer: {
+    static: {
+      directory: path.join(__dirname, './dist'),
+    },
+    hot:true,
+    client: {
+      progress: true,
+    },
+    compress: true,
+    port: 8080,
+  },
+  performance: {
+    hints: false,
+    maxEntrypointSize: 512000,
+    maxAssetSize: 512000
+  },
+  output: {
+    filename: 'bundle.[fullhash].js',
+    clean: true
+  },
+  devtool: 'inline-source-map',
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx|ts|tsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader']
+      },
+      {
+        test: /\.(png|jpe?g|svg)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              limit: 8000,
+              name: 'images/[name].[ext]'
+            }
+          }
+        ]
+      },
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: 'style-loader'
+          },
+          {
+            loader: 'css-loader',
+            options: {
+              modules: true,
+              //localsConvention: 'camelCase',
+              sourceMap: true
+            }
+          }
+        ]
+      },
+      {
+        test: /\.json$/,
+        loader: 'json-loader'
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      hash: true,
+      title: 'Poultry Market Chain - duck tracking',
+      header: 'Poultry Market Chain - duck tracking',
+      //favicon: 'src/favicon.ico',
+      template: 'src/index.html',
+      filename: 'index.html',
+      inject: 'body'
+    })
+  ],
+};
