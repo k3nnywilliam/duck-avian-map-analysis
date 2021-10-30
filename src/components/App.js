1bad6fec09438a9f205cbdb79311d3d6ad1fb5b9
Kenny Nyallau <k3nnywilliam@gmail.com>
2021-10-30 19:14:15 +0800

    Add side panels, drop down menu and fragment the elements

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,33 @@
-import React, { Component } from 'react';
-import DuckMap from './Map.js';
-import CustomIcon from "./CustomIcon.js";
-//import SidePanel from './Sidebar.js';
-import mallardicon from "../img/mallard-duck2.png";
-import muscovyicon from "../img/muscovy2.png";
+import React, { Component, Fragment } from 'react';
+
+import CustomMap from './Map.js';
+import CustomIcon from './CustomIcon.js';
+import mallardicon from '../img/mallard-duck2.png';
+import muscovyicon from '../img/muscovy2.png';
 import { geoFeaturePoints, geoFeaturesLines } from '../geo_data/GeoFeatures.js';
+import SidePane from './Sidebar.js';
+import styles from './App.css';
+import SimplePane from './SimpleSidePane.js';
+//import Dropdown from 'react-bootstrap/Dropdown';
 
 class App extends Component {
-    render(){
-        return (
-            <div><DuckMap center={[29.17384, 116.27890]} zoom={10} 
-            geofeatPoints={geoFeaturePoints} geofeatLines={geoFeaturesLines} /></div>
-        );
+    render() {
+        return [
+            <Fragment>
+
+            <div className={styles.topdiv}> TEST</div>
+                <div className={styles.middlebox}>
+                    <CustomMap center={[29.17384, 116.27890]} zoom={10}
+                        geofeatPoints={geoFeaturePoints} geofeatLines={geoFeaturesLines} />
+                </div>
+                <div className={styles.rightbox}>
+                    <SimplePane  />
+                </div>
+                
+            </Fragment>
+        ];
     }
 }
 
 export default App;
+
