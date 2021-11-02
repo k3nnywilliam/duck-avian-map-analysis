f6deb856a1641ed7bd63c1b136690c6e3e7a5368
Kenny Nyallau <k3nnywilliam@gmail.com>
2021-11-02 15:18:40 +0800

    Add navbar component with sliding panel and dropdown menu

---
+++ src/components/Navbar.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+import {Container, Navbar, Offcanvas, Nav, NavDropdown} from 'react-bootstrap';
+import navstyles from './Navbar.css';
+
+const MapNavbar = () => {
+    return (
+        < Navbar bg="light" expand={false} >
+            <Container fluid>
+                <Navbar.Brand href="#">Market Poultry Chain</Navbar.Brand>
+                <Navbar.Toggle aria-controls="offcanvasNavbar" />
+                <Navbar.Offcanvas
+                    id="offcanvasNavbar"
+                    aria-labelledby="offcanvasNavbarLabel"
+                    placement="end"
+                >
+                    <Offcanvas.Header closeButton>
+                        <Offcanvas.Title id="offcanvasNavbarLabel">Menu</Offcanvas.Title>
+                    </Offcanvas.Header>
+                    <Offcanvas.Body>
+                        <Nav className="justify-content-end flex-grow-1 pe-3">
+                            <NavDropdown title="Choose Duck Tag" id="offcanvasNavbarDropdown">
+                                <NavDropdown.Item href="#action3">Tag ID 1</NavDropdown.Item>
+                                <NavDropdown.Item href="#action4">Tag ID 2</NavDropdown.Item>
+                            </NavDropdown>
+                        </Nav>
+                    </Offcanvas.Body>
+                </Navbar.Offcanvas>
+            </Container>
+        </Navbar >
+
+    );
+};
+
+export default MapNavbar;
