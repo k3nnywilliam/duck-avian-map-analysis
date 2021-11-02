83b6702d2ca406dbaf3457a3124b1ff66e94b67c
Kenny Nyallau <k3nnywilliam@gmail.com>
2021-11-02 15:23:57 +0800

    Add direct import geojson file, make map into react component, add custom icon

--- src/components/App.js
+++ src/components/App.js
@@ -1,29 +1,16 @@
 import React, { Component, Fragment } from 'react';
-
 import CustomMap from './Map.js';
-import CustomIcon from './CustomIcon.js';
-import mallardicon from '../img/mallard-duck2.png';
-import muscovyicon from '../img/muscovy2.png';
-import { geoFeaturePoints, geoFeaturesLines } from '../geo_data/GeoFeatures.js';
-import SidePane from './Sidebar.js';
 import styles from './App.css';
-import SimplePane from './SimpleSidePane.js';
-//import Dropdown from 'react-bootstrap/Dropdown';
+import MapNavbar from './Navbar.js';
 
 class App extends Component {
     render() {
         return [
             <Fragment>
-
-            <div className={styles.topdiv}> TEST</div>
+                <MapNavbar />
                 <div className={styles.middlebox}>
-                    <CustomMap center={[29.17384, 116.27890]} zoom={10}
-                        geofeatPoints={geoFeaturePoints} geofeatLines={geoFeaturesLines} />
+                <CustomMap />
                 </div>
-                <div className={styles.rightbox}>
-                    <SimplePane  />
-                </div>
-                
             </Fragment>
         ];
     }
--- src/components/Map.js
+++ src/components/Map.js
@@ -1,12 +1,16 @@
-import React, { Component } from 'react';
+import React, { Component, useEffect, createRef } from 'react';
 import L from "leaflet";
-import { MapContainer, TileLayer, Marker, Popup, GeoJSON } from 'react-leaflet';
-//import 'leaflet/dist/leaflet.css';
+import { MapContainer, TileLayer, Marker, Popup, GeoJSON, useMap } from 'react-leaflet';
+import 'leaflet/dist/leaflet.css';
 import icon from 'leaflet/dist/images/marker-icon.png';
 import iconShadow from 'leaflet/dist/images/marker-shadow.png';
 import iconRetina from 'leaflet/dist/images/marker-icon-2x.png';
 import styles from './Map.css';
-//import points from '../geo_data/points.json';
+import points from '../geo_data/points.json';
+import lines from '../geo_data/lines.json';
+import { CustomIcon } from "./CustomIcon.js";
+import mallardicon from './images/mallard-duck2.png';
+import muscovyicon from './images/muscovy2.png';
 
 L.Icon.Default.imagePath = '../node_modules/leaflet';
 
@@ -19,6 +23,56 @@ L.Icon.Default.mergeOptions({
 });
 
 
+function MapComponent() {
+    const mapInstance = useMap();
+
+    let mallard = new CustomIcon({ iconUrl: mallardicon });
+    let muscovy = new CustomIcon({ iconUrl: muscovyicon });
+
+    let linestyle = {
+        color: 'red',
+        weight: 5,
+        opacity: 0.6
+    },
+        stroke = {
+            color: "#fff",
+            weight: 7,
+            opacity: 0.4
+        };
+
+
+    useEffect(() => {
+        if (!mapInstance) return;
+
+        const lineGeoJson = new L.geoJSON(lines, {
+            style: linestyle
+        })
+
+        const pointsGeoJson = new L.GeoJSON(points, {
+            pointToLayer: function (feature, latlng) {
+                switch (feature.properties.individual) {
+                    case 'Anas platyrhynchos': return L.marker(latlng, { icon: mallard });
+                    case 'Cairina moschata': return L.marker(latlng, { icon: muscovy });
+                }
+            },
+            onEachFeature: function (feature, layer) {
+                layer.bindPopup(`Tag ID: ${feature.properties.tag_ident}  
+                <p> Date: ${feature.properties.date}</p>`);
+            }
+        });
+
+    if (!pointsGeoJson) {
+        console.log("Can't load GeoJSON");
+    }
+
+    lineGeoJson.addTo(mapInstance);
+    pointsGeoJson.addTo(mapInstance);
+
+}, [])
+
+return null;
+}
+
 class CustomMap extends Component {
     constructor() {
         super();
@@ -26,11 +80,13 @@ class CustomMap extends Component {
 
     render() {
         return (
-            <MapContainer className={styles.map} center={this.props.center} zoom={this.props.zoom} scrollWheelZoom={true}>
-                <TileLayer attribution={`© <a href="https://www.mapbox.com/about/maps/">Mapbox</a> © <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> <strong><a href="https://www.mapbox.com/map-feedback/" target="_blank">Improve this map</a></strong>`} url="https://api.mapbox.com/styles/v1/kennyallau/ckuoxocix5u8s17nsv1gpudg6/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1Ijoia2VubnlhbGxhdSIsImEiOiJja3VveHd3Z3UwMmMxMm9xdm1uMWhlaGlwIn0.5IWi1IBZe3-ypfrW-0jivA" />
-
+            <MapContainer className={styles.map} center={[29.17384, 116.27890]} zoom={10} scrollWheelZoom={true}>
+                <TileLayer attribution={`© <a href="https://www.mapbox.com/about/maps/">Mapbox</a> © <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> <strong><a href="https://www.mapbox.com/map-feedback/" target="_blank">Improve this map</a></strong>`} url="https://api.mapbox.com/styles/v1/kennyallau/ckuoxocix5u8s17nsv1gpudg6/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1Ijoia2VubnlhbGxhdSIsImEiOiJja3VveHd3Z3UwMmMxMm9xdm1uMWhlaGlwIn0.5IWi1IBZe3-ypfrW-0jivA" 
+                    maxZoom={15}/>
+                <MapComponent />
             </MapContainer>
         );
     }
 }
+
 export default CustomMap;
